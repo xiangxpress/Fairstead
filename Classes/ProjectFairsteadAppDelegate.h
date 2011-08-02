ca228224e94c44bb5d5544f67e28736881faefdd
William Martin <wjkmartin@WilliamMartinMacbookPro.local>
2011-08-02 16:12:57 -0700

    INITAL VERSION

---
+++ Classes/ProjectFairsteadAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  ProjectFairsteadAppDelegate.h
+//  ProjectFairstead
+//
+//  Created by William Martin on 11-08-02.
+//  Copyright 2011 Mission Secondary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProjectFairsteadAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ProjectFairstead
+//
+//  Created by William Martin on 11-08-02.
+//  Copyright 2011 Mission Secondary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
